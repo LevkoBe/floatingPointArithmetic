965facec17a275ec53f914c76122f33c333d8fb8
LevkoBe <lbeniakh@kse.org.ua>
2025-02-20 23:57:44 +0200

    tests: conversion unit tests

---
+++ Unit-Tests/test.cpp
@@ -0,0 +1,55 @@
+#include "pch.h"
+#include "../floatingPointArithmetic/conversion.h"
+#include <limits>
+
+TEST(ConversionTests, FloatToBytes)
+{
+    float values[] = { 56733.3f, 0.0f, -0.0f, std::numeric_limits<float>::infinity(),
+                      -std::numeric_limits<float>::infinity(), std::numeric_limits<float>::max(),
+                      std::numeric_limits<float>::min() };
+
+    for (float value : values)
+    {
+        uint32_t bit = toBytes(value);
+        float unbit = toFloat(bit);
+        EXPECT_EQ(value, unbit);
+    }
+}
+
+TEST(ConversionTests, FloatToBytesNaN)
+{
+    uint32_t bit = toBytes(std::nanf(""));
+    float unbit = toFloat(bit);
+    EXPECT_TRUE(std::isnan(unbit));
+}
+
+TEST(ConversionTests, DoubleToBytes)
+{
+    double values[] = { 72433.3, -6.8765, 0.0, -0.0, std::numeric_limits<double>::infinity(),
+                       -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::max(),
+                       std::numeric_limits<double>::min() };
+
+    for (double value : values)
+    {
+        uint64_t bit = toBytes(value);
+        double unbit = toDouble(bit);
+        EXPECT_EQ(value, unbit);
+    }
+}
+
+TEST(ConversionTests, DoubleToBytesNaN)
+{
+    uint64_t bit = toBytes(std::nan(""));
+    double unbit = toDouble(bit);
+    EXPECT_TRUE(std::isnan(unbit));
+}
+
+TEST(ConversionTests, BruteForceCheck)
+{
+    uint32_t nu = 1;
+    float floated;
+    while (nu++ != 0) {
+        floated = toFloat(nu);
+		EXPECT_EQ(toBytes(floated), nu);
+    }
+}
--- floatingPointArithmetic/floatingPointArithmetic.cpp
+++ floatingPointArithmetic/floatingPointArithmetic.cpp
@@ -6,17 +6,18 @@ void bruteforceCheck() {
     float nuf;
     while (nu != 0) {
         nuf = toFloat(nu);
-        //if (isFinite(nuf) != std::isfinite(nuf)) std::cout << nuf << std::endl;
-        //if (isInfinity(nuf) != std::isinf(nuf)) std::cout << nuf << std::endl;
-        //if (fpclassify(nuf) != std::fpclassify(nuf)) std::cout << nuf << std::endl;
+        if (isFinite(nuf) != std::isfinite(nuf)) std::cout << nuf << std::endl;
+        if (isInfinity(nuf) != std::isinf(nuf)) std::cout << nuf << std::endl;
+        if (fpclassify(nuf) != std::fpclassify(nuf)) std::cout << nuf << std::endl;
         //if (clamp(nuf, 23.23f, 2343.23423f) != std::clamp(nuf, 23.23f, 2343.23423f)) std::cout << nuf << std::endl;
-        if (lessThan(nuf, nuf + FLT_EPSILON) != (nuf < (nuf + FLT_EPSILON))) std::cout << nuf << std::endl;
+        //if (lessThan(nuf, nuf + FLT_EPSILON) != (nuf < (nuf + FLT_EPSILON))) std::cout << nuf << std::endl;
         nu++;
     }
 }
 
 int main()
 {
-    bruteforceCheck();
+	checkStage1();
+    //bruteforceCheck();
     return 0;
 }
