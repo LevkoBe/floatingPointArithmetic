322d7d1eaa669937e1d95cbf53208151fdc3cfe7
LevkoBe <lbeniakh@kse.org.ua>
2025-02-21 11:45:07 +0200

    refactor: conversion tests

--- Unit-Tests/conversionTests.cpp
+++ Unit-Tests/conversionTests.cpp
@@ -1,56 +1,54 @@
 #include "pch.h"
 #include "../floatingPointArithmetic/conversion.h"
 #include <limits>
+#include <array>
 
-TEST(ConversionTests, FloatToBytes)
-{
-    float values[] = { 56733.3f, 0.0f, -0.0f, std::numeric_limits<float>::infinity(),
-                      -std::numeric_limits<float>::infinity(), std::numeric_limits<float>::max(),
-                      std::numeric_limits<float>::min() };
+static const std::array<float, 7> FLOAT_TEST_VALUES = {
+    56733.3f, 0.0f, -0.0f, std::numeric_limits<float>::infinity(),
+    -std::numeric_limits<float>::infinity(), std::numeric_limits<float>::max(),
+    std::numeric_limits<float>::min()
+};
 
-    for (float value : values)
-    {
+static const std::array<double, 8> DOUBLE_TEST_VALUES = {
+    72433.3, -6.8765, 0.0, -0.0, std::numeric_limits<double>::infinity(),
+    -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::max(),
+    std::numeric_limits<double>::min()
+};
+
+TEST(ConversionTests, FloatToBytes) {
+    for (float value : FLOAT_TEST_VALUES) {
         uint32_t bit = toBytes(value);
         float unbit = toFloat(bit);
         EXPECT_EQ(value, unbit);
     }
 }
 
-TEST(ConversionTests, FloatToBytesNaN)
-{
-    uint32_t bit = toBytes(std::nanf(""));
+TEST(ConversionTests, FloatToBytesNaN) {
+    float nanValue = std::nanf("");
+    uint32_t bit = toBytes(nanValue);
     float unbit = toFloat(bit);
     EXPECT_TRUE(std::isnan(unbit));
 }
 
-TEST(ConversionTests, DoubleToBytes)
-{
-    double values[] = { 72433.3, -6.8765, 0.0, -0.0, std::numeric_limits<double>::infinity(),
-                       -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::max(),
-                       std::numeric_limits<double>::min() };
-
-    for (double value : values)
-    {
+TEST(ConversionTests, DoubleToBytes) {
+    for (double value : DOUBLE_TEST_VALUES) {
         uint64_t bit = toBytes(value);
         double unbit = toDouble(bit);
         EXPECT_EQ(value, unbit);
     }
 }
 
-TEST(ConversionTests, DoubleToBytesNaN)
-{
-    uint64_t bit = toBytes(std::nan(""));
+TEST(ConversionTests, DoubleToBytesNaN) {
+    double nanValue = std::nan("");
+    uint64_t bit = toBytes(nanValue);
     double unbit = toDouble(bit);
     EXPECT_TRUE(std::isnan(unbit));
 }
 
-// for me it took 4 minutes to run this test
-TEST(ConversionTests, BruteForceCheck)
-{
+TEST(ConversionTests, BruteForceCheck) {
     uint32_t nu = 1;
-    float floated;
-    while (nu++ != 0) {
-        floated = toFloat(nu);
-		EXPECT_EQ(toBytes(floated), nu);
-    }
+    do {
+        float floated = toFloat(nu);
+        EXPECT_EQ(toBytes(floated), nu);
+    } while (++nu != 0);
 }
